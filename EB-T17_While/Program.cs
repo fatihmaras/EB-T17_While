773a14418cbe803a4bf979b6439db74b8d047afc
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 04:10:25 +0300

    While Example

---
+++ EB-T17_While/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDongusuPratik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            while (a <= 10)
+            {
+                Console.WriteLine(a);
+                a++;
+
+            }
+
+            Console.WriteLine("\n");
+            int b = 0;
+            while (b <= 100)
+            {
+
+                Console.WriteLine(b);
+                b += 2;
+
+            }
+
+            Console.WriteLine("\n");
+
+
+            int c = 100;
+            while (c >= 0)
+            {
+                Console.WriteLine(c);
+                c -= 2;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
